dc79f945bdc54bc379b817c00b837daf63c89a17
mjschrader307 <mjschrader307@gmail.com>
2025-04-15 21:57:04 -0600

    testing code

--- code.js
+++ code.js
@@ -1,3 +1,23 @@
 function convertToAdjList(adjMatrix) {
-    return adjMatrix;
+    // Initialize list
+    let adjList = [];
+    let numVertices = adjMatrix.length;
+    for (let k = 0; k < numVertices; k++) {
+        adjList.push([]);
+    }
+
+    // Iterate over rows
+    for (let j = 0; j < adjMatrix.length; j++) {
+        let L = adjMatrix[j];
+
+        // Iterate over edges (i, j)
+        for (let i = 0; i < L.length; i++) {
+            let matrixEntry = adjMatrix[j][i];
+            if (matrixEntry == 1) {
+                adjList[j].push(i)
+            }
+        }
+    }
+
+    return list;
 }
